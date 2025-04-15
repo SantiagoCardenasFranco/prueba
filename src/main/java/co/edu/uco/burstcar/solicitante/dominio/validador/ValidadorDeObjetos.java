eaf351887c0d6543cb0196fa670c6e283ccbefde
CEIBA\santiago.cardenas <santiago.cardenas@ceiba.com.co>
2025-04-15 08:58:57 -0500

    Rama de desarrollo1

--- src/main/java/co/edu/uco/burstcar/solicitante/dominio/validador/ValidadorDeObjetos.java
+++ src/main/java/co/edu/uco/burstcar/solicitante/dominio/validador/ValidadorDeObjetos.java
@@ -5,6 +5,7 @@ public class ValidadorDeObjetos {
     private ValidadorDeObjetos() {
     }
 
+    
     public static void validateNoNulos(Object valor, String nombreAtributo) {
         if(valor == null) {
             throw new IllegalArgumentException("El atributo " + nombreAtributo +" no puede estar nulo");
